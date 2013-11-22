453dbfbe63b9dfd3651d1cce9f64a612ac39d695
jeffrey <jeffsiracha76@gmail.com>
2013-11-22 12:40:01 +0700

    add files

---
+++ javascript/app.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function() {
+
+	  var toggle = $('#toggle'),
+	      wrapper = $('#wrapper, footer'),
+        // subToggleMain = $('#main-menu').children('li').next('li:nth-child(1)').children('a:nth-child(1)');
+        subToggleMain = $('#main-menu > li:nth-child(2) > a:nth-child(1)'),
+        subToggleSide = $('#side-menu > li:nth-child(2) > a:nth-child(1)'),
+        subNavMain = $('#sub-menu-main'),
+        h = subNavMain.height(),
+        subNavSide = $('#sub-menu-side');
+        subToggleMain.attr('data-icon', '\uE75C');      
+        
+        console.log(h);
+
+        toggle.bind('click', function(event) {
+          if (wrapper.hasClass('open-nav')) { 
+             
+               if (subNavMain.hasClass('open')) {
+                  subNavMain.removeClass('open');}
+           };
+               wrapper.toggleClass('open-nav');
+                 return false;
+        });
+         
+
+        subToggleMain.bind('click', function(event) {
+              subNavMain.toggleClass('open');
+              return false;
+        });
+
+        subToggleSide.bind('click', function(event) {
+              subNavSide.toggleClass('open');
+              return false;
+        });
+
+});
---
+++ javascript/map.js
@@ -0,0 +1,124 @@
+var geocoder; // currently not used
+
+$(document).ready ( function() {
+  
+  //loop through all the mapping elements
+  $(".mapping").each(function() {
+
+
+    var map;
+    var bounds;
+  
+    // initialize the map canvas
+    $(this).find('.map-canvas').each(function() {    
+      map = initMap(this);
+      bounds = new google.maps.LatLngBounds();
+    });
+
+    // loop through map points and put them on the map
+    var points = $(this).find(".map-point");
+    points.each(function() {
+
+      var latlng = new google.maps.LatLng($(this).attr('lat'), $(this).attr('long'));   
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: $(this).attr('icon'),
+        position: latlng,
+        title: $(this).find(".description").text(),
+        url: $(this).find(".description a").attr('href')
+      });
+      
+      // if the map point has HTML, turn it into an info window
+      var infowindow = new google.maps.InfoWindow({
+        content: $(this).html() 
+      });
+      
+     
+
+      $(window).resize(function() {
+
+         // if (map.zoom != 17) {
+         //     map.setZoom(17);
+         //  }
+         map.panTo(latlng);
+      });
+      
+      // handle clicks to the marker
+      google.maps.event.addListener(marker, 'click', function() {
+        if (this.url) {
+          window.open(this.url);
+        } else {
+          infowindow.open(map, marker);
+        }
+      });
+
+      // if there are multiple markers, fit the map to them; otherwise, center the map on the only marker
+      if (points.length == 1) {
+        map.setCenter(latlng);
+      } else {      
+        bounds.extend(latlng);
+        map.fitBounds(bounds);
+      }
+
+    });
+
+  });
+  
+});
+
+
+function initMap(canvas) {
+
+  // set a default zoom unless it's specified
+  zoom = parseInt($(canvas).attr('zoom')) || 18;
+
+  // center the map and create options
+  var latlng = new google.maps.LatLng(latlng);
+  var myOptions = {
+      center: new google.maps.LatLng(latlng),
+      zoom: 17,
+      scrollwheel: false,
+      scaleControl: false,
+      mapTypeIds: [google.maps.MapTypeId.HYBRID, 'SolidXpress_map']
+    };
+
+  // create the map
+  map = new google.maps.Map(canvas, myOptions);
+  map.setCenter(latlng);
+  
+  // set some custom styles
+  map.set('styles', [
+    {
+      featureType: 'landscape',
+      elementType: 'all',
+      stylers: [
+        { hue: "#DE0C00"}
+      ]
+    }, {
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [
+        { hue: "#444444"},
+        { saturation: -20}
+      ]
+    },{
+      featureType: 'water',
+      elementType: 'geometry.fill',
+      stylers: [
+        { color: "#222"},
+        { lightness: 44 },
+        { saturation: -45 }
+      ]
+    },{
+    featureType: 'road.highway',
+      elementType: 'all',
+      stylers: [
+        { hue: "#DE0C00"},
+        { saturation: -70}
+      ]
+    }
+  ]);
+  
+  return map;
+  
+}
